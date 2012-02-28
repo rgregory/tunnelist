4a4e8041893eef948c36c82329260eced4f10038
Roger Gregory <rtgregory@gmail.com>
2012-02-28 11:07:50 -0500

    First draft of genUser script; adding cfg for single purpose SMTP daemon

---
+++ _system/scripts/genUser.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+USERNAME=$1
+USEREMAIL=$2
+PASSWORD=$3
+TOKEN=$4
+EXPIRE=""
+GROUP="customers"
+SHELL="/usr/local/bin/rshell"
+AU="/usr/sbin/adduser"
+PSWD="/usr/bin/passwd"
+
+# Sanity checks
+#
+if [ ! `grep rshell /etc/shells` ]; then
+     exit 1
+fi
+
+# Create user
+#
+$AU -C "$USEREMAIL,$TOKEN" -f 1 -M -e $EXPIRES -g $GROUP -n -s $SHELL $USERNAME
+echo "$PASSWORD" | passwd --stdin $USERNAME
