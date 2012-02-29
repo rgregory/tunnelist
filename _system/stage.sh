1e12ab07d986f38b76253ec2712c808e55d17a30
Roger Gregory <rtgregory@gmail.com>
2012-02-28 20:22:45 -0500

    Adding staging script to move cfg

---
+++ _system/stage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+[ -e /command/ ] || ( echo "Need Daemontools" && exit 1)
+
+DEPLOY="createKey.sh firewall.sh genUser.sh rshell"
+
+for all in $DEPLOY; do
+	cp -v scripts/$all /usr/local/bin/$all
+done 
+
+if [ ! `grep rshell /etc/shells` ]; then
+     echo "/usr/local/bin/rshell" >> /etc/shells
+fi
+
+mkdir /etc/tunnelist
+
+mv /etc/security/limits.conf /etc/security/limits.conf.orig
+cp -v cfg/limits.conf /etc/security/limits.conf
+mv /etc/login.defs /etc/login.defs.orig
+cp -v cfg/logins.defs /etc/login.defs
+cp -v cfg/sshd_config /etc/tunnelist/
+cp -v cfg/ssmtp.conf /etc/
+mv /etc/sysctl.conf /etc/sysctl.conf.orig
+cp -v cfg/sysctl.conf /etc/sysctl.conf
+
+# Firewall
+# 
+chmod +x /usr/local/bin/*
+echo "/usr/local/bin/firewall.sh" >> /etc/rc.local
+
+# Daemontools
+#
+cp -rv supervised/* /etc/tunnelist/
+chmod +x /etc/tunnelist/run /etc/tunnelist/log/run
